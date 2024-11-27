711808deda786739f1adf2b0a9adc6eb4cf59ef3
depy <42985524+h4ckdepy@users.noreply.github.com>
2024-11-27 20:03:47 +0800

    Update store2mysql.go
    1、修改protobuf结构，增加reqType字段，不再从字符串切割
    2、增加插件Topic字段，用于插件载入时check

--- examples/store2mysql.go
+++ examples/store2mysql.go
@@ -9,7 +9,6 @@ import (
 	"encoding/hex"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
-	"strings"
 	"sync"
 	"time"
 )
@@ -56,20 +55,21 @@ func (p *MyPlugin) ProcessBegin(data *Proto.NetworkData, data2 interface{}, data
 	} else {
 
 		go func() {
+
 			isChunkedInt := 0
 			if data.IsChunked {
 				isChunkedInt = 1
 			}
 
-			//存储抹掉TraceID请求类型前缀标志 方便后期定位索引
 			TraceID := data.TraceID
-			TraceID = strings.TrimPrefix(TraceID, "REQ-")
-			TraceID = strings.TrimPrefix(TraceID, "RES-")
 
-			ReqType := 1
-			if strings.HasPrefix(data.TraceID, "RES") {
+			ReqType := 0
+			if data.ReqType == "REQ"{
+				ReqType = 1
+			}else{
 				ReqType = 0
 			}
+
 			timestampSec := time.Now().Unix()
 			query := `INSERT INTO cd_flows (TraceID, RawData, ChunkNum, IsChunked, ServiceHost, ServicePort, reqType,node,create_time) VALUES (?, ?, ?, ?, ?, ?, ?,?,?) `
 
@@ -92,13 +92,12 @@ func (p *MyPlugin) ProcessEnd(data *Proto.NetworkData, data2 interface{}, data3 
 	} else {
 
 		TraceID := data.TraceID
-		TraceID = strings.TrimPrefix(TraceID, "REQ-")
-		TraceID = strings.TrimPrefix(TraceID, "RES-")
 
 		timestampSec := time.Now().Unix()
 
 		query := `INSERT INTO cd_regexresult (TraceID, node,matchedText, regexgroups_id, regexrules_id,create_time) VALUES (?,?, ?, ?, ?,?)`
 
+		//_, err := db.Exec(query, TraceID, data2.(string), hex.EncodeToString([]byte(data3.(string))), data5.(Config.Rule).IDGroup, data5.(Config.Rule).IDRule, timestampSec)
 		_, err := db.Exec(query, TraceID, data2.(string), data3.(string), data5.(Config.Rule).IDGroup, data5.(Config.Rule).IDRule, timestampSec)
 
 		if err != nil {
@@ -115,11 +114,15 @@ func (p *MyPlugin) Name() string {
 }
 
 func (p *MyPlugin) Version() string {
-	return "1.3"
+	return "1.5"
 }
 
 func (p *MyPlugin) Author() string {
 	return "depy"
+}
+
+func (p *MyPlugin) Topic() string {
+	return "test"
 }
 
 func RegisterPlugin() PluginInterface.Processor {
